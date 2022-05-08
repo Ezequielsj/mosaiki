047aa17dc578474aaf29ac4b7bc353d731ed25e1
Ezequiel Santos <ezequielsantos@poli.ufrj.br>
2022-05-07 22:39:32 -0300

    Login

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,8 +1,164 @@
 import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import LockSharpIcon from '@mui/icons-material/LockSharp';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import Link from '@mui/material/Link';
+import { textAlign } from '@mui/system';
+
+
+
+const useStyles = makeStyles((createTheme) => ({
+    root: {
+        height: '100vh'
+    },
+    
+    img: {
+        maxHeight: 270,  
+     },
+
+     Button: {
+         marginTop: 15,
+     },
+
+     form: {
+         marginRight: '8%',
+         marginLeft: '16%', 
+        
+     },
+
+     image: {
+         padding: '16',
+         textAlign: 'center',
+         
+     }
+
+
+}));
 
 function SignIn() {
+    const classes = useStyles(); 
+
     return (
-        <h1>Login Component</h1>
+        <Grid container className={classes.root}>
+            <Grid
+             item 
+             container
+             direction="column"
+             justifyContent="center"
+             alignItems="center"
+             backgroundColor= 'Black'
+             md={7}>
+
+            <image className={classes.image}>
+                <img src="/images/logoBlack.jpeg" alt="logo" className={classes.img} />
+                <Typography style={{color: '#fff', fontSize: 50, lineHeight: '50px'}}>
+                   <strong>Mosaiki</strong>
+                </Typography>
+
+                <Typography variant="body2" style={{color: 'rgb(255,255,255, 0.7)', marginTop: 30, fontSize: 25, lineHeight: '30px'}}>
+                    Dois pensam melhor do que um, mil pensam melhor do que dois.
+                </Typography>
+            </image>
+
+            </Grid>
+
+            <Grid item md={5}>
+                <Box display="flex" flexDirection="column" alignItems="center" mt={8} >
+                    {/* <Avatar className={classes.avatar} > */}
+
+                    <LockSharpIcon color='primary' sx={{ fontSize: 40 }}/>
+                    
+
+                    {/* </Avatar> */}
+                    
+                    <Typography variant='h6'>
+                        <strong>Acesso</strong>
+                    </Typography>
+
+                    <form className={classes.form}>
+                        <TextField
+                        variant='outlined'
+                        margin='normal'
+                        required
+                        fullWidth
+                        id='email'
+                        label='E-mail'
+                        name='email'
+                        autoComplete='email'
+                        autoFocus
+                        />
+
+                        <TextField
+                        variant='outlined'
+                        margin='normal'
+                        required
+                        fullWidth
+                        id='password'
+                        label='Senha'
+                        type='password'
+                        name='password'
+                        autoComplete='current-password'
+                        />
+                        <div className={classes.Button}>
+                        <Button
+                        
+                        variant='contained'
+                        color='primary'
+                        fullWidth>
+                            Entrar
+                        </Button>
+                        </div>
+
+                        <Grid container>
+                            <Grid item>
+                                <Link>Esqueceu sua senha?</Link>
+                            </Grid>
+                            <Grid item>
+                                <Link>Não tem uma conta? registrar</Link>
+                            </Grid>
+                            
+
+
+                        </Grid>
+
+                    </form>
+
+                </Box>
+
+            </Grid>
+
+
+
+
+        </Grid>
+        
+        /* <div className={classes.root}>
+            <div className={classes.left}>
+
+                <img src="/images/logoBlack.jpeg" alt="logo" className={classes.img} />
+                <Typography style={{color: '#fff', fontSize: 50, lineHeight: '50px'}}>
+                   <strong>Mosaiki</strong>
+                </Typography>
+
+                <Typography variant="body2" style={{color: 'rgb(255,255,255, 0.7)', marginTop: 30, fontSize: 25, lineHeight: '30px'}}>
+                    Dois pensam melhor do que um, mil pensam melhor do que dois.
+                </Typography>
+            </div>
+
+            <div className={classes.right}>
+                <form className={classes.form}>
+                    <h4>Acesso</h4>
+                    <input type="text" />
+                    <input type="text" />
+
+                </form>
+            </div>
+        </div> */
     )
 }
 
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,44 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 import palette from './palette';
+import typography from './typography';
 
 const theme = createTheme ({
     palette,
+    typography
 
-    typography: {
-        h1: {
-            fontWeight: 500,
-            fontSize: 35,
-            letterSpacing: '-0,24px',
-        },
-        h2: {
-            fontWeight: 500,
-            fontSize: 29,
-            letterSpacing: '-0,24px',
-        },
-        h3: {
-            fontWeight: 500,
-            fontSize: 24,
-            letterSpacing: '-0,06px',
-        },
-        h4: {
-            fontWeight: 500,
-            fontSize: 20,
-            letterSpacing: '-0,005px',
-        },
-        h5: {
-            fontWeight: 500,
-            fontSize: 16,
-            letterSpacing: '-0,005px',
-        },
-        h6: {
-            fontWeight: 500,
-            fontSize: 14,
-            letterSpacing: '-0,005px',
-        },
-
-
-    
-    }
 })
 
 export default theme;
